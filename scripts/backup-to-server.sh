1d8f09b79b4af50ee5d648a2d3443be6c3de1d08
L <24641464+lz-eng@users.noreply.github.com>
2022-05-23 14:19:26 +1200

    Create backup-to-server.sh

---
+++ scripts/backup-to-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Example to rsync backup your UDM-PRO to a server.
+## Replace username and servername with your own.
+## Place the script on your UDM-PRO
+## Can be a cronjob scheduled daily, weekly or similar 
+
+## start unifi-os shell and store script in home directory
+## commands un-comment
+
+unifi-os shell
+cd ~
+
+rsync -av /data/unifi/data/backup/autobackup/*.unf username@servername:unifi-backup/
